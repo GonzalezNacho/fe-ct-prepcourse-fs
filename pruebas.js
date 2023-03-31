c387e78e9ecf99cb29baf4d4d5b123a9b1ed25a0
GonzalezNacho <nacho.ldm63@gmail.com>
2023-03-31 12:25:26 -0300

    modulo 7 terminado

--- pruebas.js
+++ pruebas.js
@@ -1,4 +1,16 @@
-let numero = {
-    letras : ["a","e"]
-};
-console.log(numero.hasOwnProperty("email"));
+class Persona {
+    // Crea el constructor de la clase "Persona".
+    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+    // sus valores.
+    constructor (nombre, apellido, edad, domicilio) {
+       this.nombre = nombre;
+       this.apellido = apellido;
+       this.edad = edad;
+       this.domicilio = domicilio;
+    }
+ 
+}
+
+let prueba = new Persona( "matias","gomez",14,"calle falsa 123");
+console.log(prueba);
